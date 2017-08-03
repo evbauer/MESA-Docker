bf122cf54a443231d14b056ff1403102b6538090
Evan Bauer <branbauep@gmail.com>
2017-08-03 00:09:16 -0700

    another try

--- .start_ssh_for_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo service ssh start
-sleep infinity
--- win_dockerMESA.sh
+++ win_dockerMESA.sh
@@ -5,12 +5,14 @@ export DISPLAY=localhost:0.0
 export HERE=$(echo $PWD | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
 
 winpty docker run -d --rm \
+       --name mesa_dock
        -p 6158:22 \
        -v $HERE/docker_work:/home/docker/docker_work \
        evbauer/mesa9793_installed:0.2 \
-       ./.start_ssh_for_docker.sh
+       sleep infinity
+
+winpty docker exec --user root mesa_dock service ssh start
 
 ssh -X -p 6158 docker@localhost
 
-# Should probably find a more elegant way to clean up eventually.
-yes | docker container prune
+docker kill mesa_dock
