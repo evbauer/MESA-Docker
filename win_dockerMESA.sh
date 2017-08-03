f428a653b95062ddc9365fcb7fbc6372418873c1
Evan Bauer <branbauep@gmail.com>
2017-08-02 21:48:24 -0700

    windows fix

--- win_dockerMESA.sh
+++ win_dockerMESA.sh
@@ -3,7 +3,7 @@ export DISPLAY=localhost:0.0
 # Needs a windows style path to mount.
 export HERE=$(echo $PWD | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
 
-docker run -d --rm \
+winpty docker run -d --rm \
        -p 6158:22 \
        -v $HERE/docker_work:/home/docker/docker_work \
        evbauer/mesa9793_installed:0.2
