312f242172f716e458339107f0681d2d0e14f4b6
Evan Bauer <branbauep@gmail.com>
2017-08-11 16:00:32 -0700

    better docker machine management for toolbox

--- home_dockerMESA.sh
+++ home_dockerMESA.sh
@@ -2,24 +2,32 @@
 
 export DISPLAY=localhost:0.0
 
-# Creating a machine with 2GB of RAM and 2 CPUs
+#Check to see if mesa-machine exists
+MACHINE_EXISTS=$(docker-machine ls | grep mesa-machine -c)
+#If not create machine, otherwise start it
+if [ $MACHINE_EXISTS -eq 0 ]
+then
+    # Creating a machine with 2GB of RAM and 2 CPUs
+    docker-machine create \
+		   -d virtualbox \
+		   --virtualbox-memory=2048 \
+		   --virtualbox-cpu-count=2 \
+		   --virtualbox-disk-size=10000 \
+		   mesa-machine
+    
+    echo "MESA MACHINE CREATED"
+else
+    docker-machine start mesa-machine
+    echo "MESA MACHINE STARTED"
+fi
 
-docker-machine create \
-	       -d virtualbox \
-	       --virtualbox-memory=2048 \
-	       --virtualbox-cpu-count=2 \
-	       --virtualbox-disk-size=10000 \
-	       mesa-machine
-
-echo "MESA MACHINE CREATED"
+#Connect terminal to the docker machine to allow running docker commands.
+eval "$(docker-machine env mesa-machine)"
 
 # Needs a windows style path to mount.
 #export HERE=$(echo $PWD | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
 #figure out the voume mounting later
 #-v $HERE/docker_work:/home/docker/docker_work \
-
-#Connect terminal to the docker machine to allow running docker commands.
-eval "$(docker-machine env mesa-machine)"
 
 docker run -d --rm \
        --name mesa_dock \
@@ -35,4 +43,5 @@ ssh -Nf -L20000:localhost:6158 docker@$ip
 # ssh with X11 forwarding for pgstar.
 ssh -Y -p 20000 docker@localhost
 
-#Do something here to cleanly close down the VM.
+docker kill mesa_dock
+docker-machine stop mesa-machine
