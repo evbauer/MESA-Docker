9fb21fa88a2f9e63a67794216f8d7a74ea247c88
Evan <branbauep@gmail.com>
2017-08-30 14:42:03 -0700

    fix for linux

--- linux_dockerMESA.sh
+++ linux_dockerMESA.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm \
-       -e DISPLAY=localhost:0 \
+       -e DISPLAY \
        -v $PWD/docker_work:/home/docker/docker_work \
+       -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
        evbauer/mesa_lean:9793.01
